c3924dbd1aecb802f77cb0838f21ce83a4f2ad95
Atharv Ajit Patil <157405700+AtharvPatil3990@users.noreply.github.com>
2025-07-26 17:31:58 +0530

    Fixed bug in SettingsFragment.java

--- app/src/main/java/com/example/cyberqrscan/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/cyberqrscan/ui/settings/SettingsFragment.java
@@ -9,24 +9,49 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.ListPreference;
 
 import com.example.cyberqrscan.databinding.FragmentSettingsBinding;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     private FragmentSettingsBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
         SettingsViewModel settingsViewModel =
                 new ViewModelProvider(this).get(SettingsViewModel.class);
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+         setPreferencesFromResource(R.xml.settings_preference, rootKey);
+        
+        // ListPreference by key
+        ListPreference themePreference = findPreference("prefAppTheme");
 
+        if (themePreference != null) {
+            // Set the listener
+            themePreference.setOnPreferenceChangeListener((preference, newValue) -> {
+                String selectedTheme = (String) newValue;
 
-
-        return root;
+                switch (selectedTheme) {
+                    case "light":
+                    // light theme    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        break;
+                    case "dark":
+                    // dark theme    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        break;
+                    
+                    default:
+                    // system_default theme AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                        break;
+                }
+                return true;
+           });
+       }
+        
     }
 
     @Override
