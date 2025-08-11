3dd73ce3586c15e399201a931fe94721f282d852
Atharv Patil <atharvapatil3990@gmail.com>
2025-08-11 13:07:45 +0530

    Create table for hash prefix

--- app/src/main/java/com/example/cyberqrscan/QRDatabase.java
+++ app/src/main/java/com/example/cyberqrscan/QRDatabase.java
@@ -6,11 +6,18 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+//CREATE TABLE url_hash_prefixes (
+//        id INTEGER PRIMARY KEY AUTOINCREMENT,
+//
+//);
+
+
 public class QRDatabase extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "cyberQRScan.db";
     private static final int DATABASE_VERSION = 1;
     public static final String scanTable = "ScanHistory" ;
     public static final String generateTable = "GenerateHistory" ;
+    public static final String urlHashTable = "URL_Hash_Table";
     public static final String type = "type";
     public static final String data = "data";
     public static final String timestamp = "timestamp";
@@ -24,6 +31,11 @@ public class QRDatabase extends SQLiteOpenHelper {
             data + " TEXT, " +
             timestamp + " INTEGER" +
             ")";
+    private static final String createTableURLHash = "CREATE TABLE " + urlHashTable +
+            " ( id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+            "    hash_prefix TEXT NOT NULL,\n" +
+            "    threat_type TEXT NOT NULL,\n" +
+            "    expiration_time INTEGER NOT NULL )";
     public QRDatabase(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -32,14 +44,25 @@ public class QRDatabase extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(createTableScan);
         sqLiteDatabase.execSQL(createTableGenerate);
+        sqLiteDatabase.execSQL(createTableURLHash);
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + scanTable);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + generateTable);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + urlHashTable);
         onCreate(sqLiteDatabase);
     }
+
+    public void UpdateURLHash(String hash, String threat_type, long expirationTime){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("hash_prefix", hash);
+        values.put("threat_type", threat_type);
+        values.put("expiration_time", expirationTime);
+        db.insert(urlHashTable, null, values);
+    }
+
     public long insertData(String scanType, String scanData, long scanTimestamp , String tableName) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
---
+++ app/src/main/java/com/example/cyberqrscan/SafeBrowsingUpdater.java
@@ -0,0 +1,4 @@
+package com.example.cyberqrscan;
+
+public class SafeBrowsingUpdater {
+}
